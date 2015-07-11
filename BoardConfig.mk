c35a91cb40deb39848ade9f538bfd19df595ba4a
kostoulhs <MrKostoulhs@gmail.com>
2015-07-11 22:41:02 +0300

    nfc: force rewrite the config
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,10 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/expressltexx/bluetooth
 
+# NFC
+#BOARD_HAVE_NFC := true
+#BOARD_NFC_CHIPSET := pn547
+#BOARD_NFC_LPM_LOSES_CONFIG := true
 
 # Audio
 BOARD_HAVE_SAMSUNG_AUDIO := true
