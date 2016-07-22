6259df51a5cf496f558bf3dd0c57fa09dfc12309
kostoulhs <MrKostoulhs@gmail.com>
2016-07-22 13:00:20 +0200

    switch back to stock browser

--- device.mk
+++ device.mk
@@ -142,10 +142,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
-# Gello
-PRODUCT_PACKAGES += \
-    Gello
-
 # call common express system props
 $(call inherit-product, device/samsung/expressltexx/system_prop.mk)
 
