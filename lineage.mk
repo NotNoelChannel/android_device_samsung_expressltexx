772e6d00e8754432d42897426c82739e4960b58c
kostoulhs <MrKostoulhs@gmail.com>
2017-12-10 04:35:24 +0100

    rebrand to lineage

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/expressltexx/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_expressltexx
 PRODUCT_DEVICE := expressltexx
