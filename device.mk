77a09fb1a20a8c7468f40b77e3d56ce0bd58df80
Bruno Martins <bgcngm@gmail.com>
2019-05-25 01:42:01 +0200

    serrano-common: Replace KeyDisabler class with Touch HAL
    Change-Id: I7d647ed282b7e0707f1fd65a74dcf6401dc9a448

--- device.mk
+++ device.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service.express
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.serrano
+
 # IPv6 tethering
 PRODUCT_PACKAGES += \
     ebtables
