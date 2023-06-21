a294f642dd5199f729ffc9d34bb1fcb7aff904ea
NotNoelChannel <notnoel12345678@gmail.com>
2023-06-21 19:22:05 +0200

    edit

--- hidl/livedisplay/service.cpp
+++ hidl/livedisplay/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.serrano"
+#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.express"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
